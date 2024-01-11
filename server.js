7796e1d12f54426a1eb9fa6a33efaa17b5c090cd
FilipBecker <filip.becker@edu.huddinge.se>
2024-01-11 14:34:11 +0100

    Fixat en extern js fil.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/exercise2.js', (req, res) => {
+    res.sendFile(path.join(__dirname + '/exercise2.js'));
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
